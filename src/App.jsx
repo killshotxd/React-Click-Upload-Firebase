a35dc378e820a3a545de23e46cd5e1be59c4bb70
killshotxd <hassanansari211@gmail.com>
2022-08-16 14:55:26 +0530

    styling

--- src/App.jsx
+++ src/App.jsx
@@ -30,28 +30,30 @@ const App = () => {
 
   return (
     <>
-      <div className="center-div">
-        <div className="input-div">
-          <input
-            className="input"
-            type="file"
-            onChange={(event) => {
-              setImageUpload(event.target.files[0]);
-            }}
-          />
+      <div className="container">
+        <div className="center-div">
+          <div className="input-div">
+            <input
+              className="input"
+              type="file"
+              onChange={(event) => {
+                setImageUpload(event.target.files[0]);
+              }}
+            />
+          </div>
+
+          <div className="btn-div">
+            <button className="input" onClick={uploadImage}>
+              Upload Image
+            </button>
+          </div>
         </div>
 
-        <div className="btn-div">
-          <button className="btn" onClick={uploadImage}>
-            Upload Image
-          </button>
+        <div className="image-card">
+          {imageList.map((url) => {
+            return <img src={url} />;
+          })}
         </div>
-      </div>
-
-      <div className="image-card">
-        {imageList.map((url) => {
-          return <img src={url} />;
-        })}
       </div>
     </>
   );
