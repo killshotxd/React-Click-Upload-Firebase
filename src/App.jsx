5051a6dc64d114b178c57ccda37a0ceb4d167462
killshotxd <hassanansari211@gmail.com>
2022-08-16 14:24:52 +0530

    setting up image upload function

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,34 @@
 import React from "react";
-
+import { useState } from "react";
+import { storage } from "./Firebase";
+import { ref, uploadBytes } from "firebase/storage";
+import { v4 } from "uuid";
 const App = () => {
-  return <div>App</div>;
+  const [imageUpload, setImageUpload] = useState(null);
+  const uploadImage = () => {
+    if (imageUpload == null) return;
+    const imageRef = ref(storage, `images/${imageUpload.name + v4()}`);
+    uploadBytes(imageRef, imageUpload).then(() => {
+      alert("Image Uploaded");
+    });
+  };
+
+  return (
+    <div>
+      <div className="input-div">
+        <input
+          type="file"
+          onChange={(event) => {
+            setImageUpload(event.target.files[0]);
+          }}
+        />
+      </div>
+
+      <div className="btn-div">
+        <button onClick={uploadImage}>Upload Image</button>
+      </div>
+    </div>
+  );
 };
 
 export default App;
